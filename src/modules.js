480e2c29148e12b42e8131cf690fed5e5a1ca0ea
nickozy <104327959+nickozy@users.noreply.github.com>
2023-05-08 01:12:39 +0300

    we

--- src/modules.js
+++ src/modules.js
@@ -3,3 +3,4 @@ export { default as Scroll } from "./modules/Scroll";
 export { default as Nav } from "./modules/Nav";
 export { default as Slider } from "./modules/Slider";
 export { default as Split } from "./modules/Split";
+export { default as Modal } from "./modules/Modal";
